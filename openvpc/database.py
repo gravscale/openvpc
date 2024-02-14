b5807f9d013955ab3c99330df334857c338a9312
Fabrício Radtke <fabricio.radtke@under.com.br>
2024-02-14 17:21:43 -0300

    dependencias ajustadas

--- openvpc/database.py
+++ openvpc/database.py
@@ -1,12 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, "dev", ".env"))
-#load_dotenv("./dev/.env")
+# load_dotenv("./dev/.env")
 
 USERNAME = os.environ.get("MYSQL_USER")
 PASSWORD = os.environ.get("MYSQL_PASSWORD")
@@ -15,10 +15,9 @@ PORT = os.environ.get("MYSQL_PORT", "3306")
 DB = os.environ.get("MYSQL_DATABASE")
 DATABASE_URL = f"mysql+aiomysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 DATABASE_URL_SYNC = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}"
-#print("################", DATABASE_URL_SYNC)
+# print("################", DATABASE_URL_SYNC)
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
-
-
--- openvpc/devices/vyos.py
+++ openvpc/devices/vyos.py
@@ -1,321 +1,389 @@
-from vyapi.device import VyDevice, ApiResponse
-
 import pprint
 
+from pyvyos.device import VyDevice
+
+
 class Device_Vyos:
-    def __init__(self, host, port, protocol, username=None, password=None, private_key=None, verify=False, timeout=10):
-        self.device = VyDevice(hostname=host, key=password, port=port, protocol=protocol, verify=verify)
-        
+    def __init__(
+        self,
+        host,
+        port,
+        protocol,
+        username=None,
+        password=None,
+        private_key=None,
+        verify=False,
+        timeout=10,
+    ):
+        self.device = VyDevice(
+            hostname=host, key=password, port=port, protocol=protocol, verify=verify
+        )
 
     @classmethod
-    def instance(cls, host, port, protocol, username=None, password=None, private_key=None, verify=False, timeout=10):
+    def instance(
+        cls,
+        host,
+        port,
+        protocol,
+        username=None,
+        password=None,
+        private_key=None,
+        verify=False,
+        timeout=10,
+    ):
         return cls(host, port, protocol, username, password, private_key, verify, timeout)
 
     def show(self, path):
         return self.device.retrieve_show_config(path)
 
     def is_connected(self):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
 
     def create_vpc(self, vpc_name, primary_device_name, secondary_device_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_vpc(self, vpc_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def get_vpc(self, vpc_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def get_vpcs(self):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def get_vpc_status(self, vpc_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def set_interface_vpc(self, vpc_name, interface_name, vlans = []):
-        response = self.show(['system'])
+
+    def set_interface_vpc(self, vpc_name, interface_name, vlans=[]):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def unset_interface_vpc(self, vpc_name, interface_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def add_gateway(self, vpc_name, gateway_name, gateway_ip, gateway_interface):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def add_route(self, vpc_name, route_name, route_ip, route_interface, route_gateway):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_gateway(self, vpc_name, gateway_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_route(self, vpc_name, route_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def create_sg(self, vpc_name, sg_name, sg_rules = []):
-        response = self.show(['system'])
+
+    def create_sg(self, vpc_name, sg_name, sg_rules=[]):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_sg(self, vpc_name, sg_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_sg_rule(self, vpc_name, sg_name, sg_rule_name, sg_rule_protocol, sg_rule_port, sg_rule_source_ip, sg_rule_destination_ip):
-        response = self.show(['system'])
+
+    def add_sg_rule(
+        self,
+        vpc_name,
+        sg_name,
+        sg_rule_name,
+        sg_rule_protocol,
+        sg_rule_port,
+        sg_rule_source_ip,
+        sg_rule_destination_ip,
+    ):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_sg_rule(self, vpc_name, sg_name, sg_rule_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_dhcp(self, vpc_name, dhcp_name, dhcp_ip_range, dhcp_gateway, dhcp_dns_servers = [], dhcp_domain_name = None, dhcp_lease_time = None, dhcp_domain_search = []):
-        response = self.show(['system'])
+
+    def add_dhcp(
+        self,
+        vpc_name,
+        dhcp_name,
+        dhcp_ip_range,
+        dhcp_gateway,
+        dhcp_dns_servers=[],
+        dhcp_domain_name=None,
+        dhcp_lease_time=None,
+        dhcp_domain_search=[],
+    ):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_dhcp(self, vpc_name, dhcp_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def add_dhcp_dns_server(self, vpc_name, dhcp_name, dns_server_ip):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_dhcp_dns_server(self, vpc_name, dhcp_name, dns_server_ip):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def add_dhcp_domain_search(self, vpc_name, dhcp_name, domain_search_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_dhcp_domain_search(self, vpc_name, dhcp_name, domain_search_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_lag(self, vpc_name, lag_name, lag_interfaces = []):
-        response = self.show(['system'])
+
+    def add_lag(self, vpc_name, lag_name, lag_interfaces=[]):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_lag(self, vpc_name, lag_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_lag_interface(self, vpc_name, lag_name, lag_interface_name, lag_interface_mode, lag_interface_speed, lag_interface_vlan = None, lag_interface_ip = None, lag_interface_description = None):
-        response = self.show(['system'])
+
+    def add_lag_interface(
+        self,
+        vpc_name,
+        lag_name,
+        lag_interface_name,
+        lag_interface_mode,
+        lag_interface_speed,
+        lag_interface_vlan=None,
+        lag_interface_ip=None,
+        lag_interface_description=None,
+    ):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_lag_interface(self, vpc_name, lag_name, lag_interface_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def set_lag_interface_vlan(self, vpc_name, lag_name, lag_interface_name, lag_interface_vlan):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def unset_lag_interface_vlan(self, vpc_name, lag_name, lag_interface_name, lag_interface_vlan):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def set_lag_interface_ip(self, vpc_name, lag_name, lag_interface_name, lag_interface_ip):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def unset_lag_interface_ip(self, vpc_name, lag_name, lag_interface_name, lag_interface_ip):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_vrrp(self, vpc_name, vrrp_name, vrrp_priority, vrrp_ip, vrrp_interface, vrrp_description = None, vrrp_track = []):
-        response = self.show(['system'])
+
+    def add_vrrp(
+        self,
+        vpc_name,
+        vrrp_name,
+        vrrp_priority,
+        vrrp_ip,
+        vrrp_interface,
+        vrrp_description=None,
+        vrrp_track=[],
+    ):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_vrrp(self, vpc_name, vrrp_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def set_vrrp_priority(self, vpc_name, vrrp_name, vrrp_priority):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def unset_vrrp_priority(self, vpc_name, vrrp_name, vrrp_priority):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    def add_nat(self, vpc_name, nat_name, nat_type, nat_interface, nat_source_ip, nat_destination_ip, nat_description = None, nat_port_forwarding = []):
-        response = self.show(['system'])
+
+    def add_nat(
+        self,
+        vpc_name,
+        nat_name,
+        nat_type,
+        nat_interface,
+        nat_source_ip,
+        nat_destination_ip,
+        nat_description=None,
+        nat_port_forwarding=[],
+    ):
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def delete_nat(self, vpc_name, nat_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def sg_associate(self, vpc_name, sg_name, interface_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
+
     def sg_dissociate(self, vpc_name, sg_name, interface_name):
-        response = self.show(['system'])
+        response = self.show(["system"])
         pprint.pprint(response)
 
-        if response.error == False:
+        if response.error is False:
             return True
         return False
-    
-    
