015f117345d8b212a75deb56f2ae494f9cc9c323
Fabrício Radtke <fabricio.radtke@under.com.br>
2024-02-20 18:58:03 -0300

    test routes

--- openvpc/routes/test_routes.py
+++ openvpc/routes/test_routes.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 from fastapi import APIRouter, HTTPException
 
 from ..devices.factory import Device_Factory
@@ -9,13 +10,22 @@ router = APIRouter()
 
 @router.get("/test")
 async def test():
-    kw = {
-        "type": "vyos",
-        "protocol": "http",
-        "host": os.environ.get("VYOS_HOST"),
-        "port": os.environ.get("VYOS_PORT"),
-        "username": os.environ.get("VYOS_USERNAME"),
-        "password": os.environ.get("VYOS_PASSWORD"),
+    # kw = {
+    #     "type": "vyos",
+    #     "protocol": "https",
+    #     "host": os.environ.get("VYOS_HOST"),
+    #     "port": os.environ.get("VYOS_PORT"),
+    #     "username": os.environ.get("VYOS_USERNAME"),
+    #     "password": os.environ.get("VYOS_PASSWORD"),
+    # }
+    # factory = Device_Factory().instance(**kw)
+    # return factory.is_connected()
+
+    url = "http://192.168.0.18:2081/retrieve"
+    payload = {
+        "data": '{"op": "showConfig", "path": []}',
+        "key": "vyos",
     }
-    factory = Device_Factory().instance(**kw)
-    return factory.is_connected()
+    headers = {}
+    response = requests.post(url, data=payload, headers=headers)
+    print(response.text)
